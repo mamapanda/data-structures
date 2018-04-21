f76fab6089c5f6f53c4f3e91c4b11d292b261e3a
panda <daniel.phan36@gmail.com>
2018-04-20 21:05:00 -0700

    added binary search tree (untested)

--- ts/collection.ts
+++ ts/collection.ts
@@ -1,20 +1,20 @@
 namespace Collections {
     export interface Collection<T> {
-        add(value: T): boolean;
+        add(value: T): void;
         clear(): void;
         empty(): boolean;
-        eraseAt(position: Iterator<T>): boolean;
+        eraseAt(position: Iterator<T>): void;
         find(value: T): Iterator<T>;
-        find(value: T, equal: (lhs: T, rhs: T) => boolean): Iterator<T>;
         iterator(): Iterator<T>;
         size(): number;
     }
 
     export interface Iterator<T> {
-        next(i?: number): boolean;
-        nextIterator(i?: number): Iterator<T>;
-        previous(i?: number): boolean;
-        previousIterator(i?: number): Iterator<T>;
+        back(): void;
+        forward(): void;
+        hasNext(): boolean;
+        hasPrevious(): boolean;
+        source(): Collection<T>;
         valid(): boolean;
         value(): T;
     }
@@ -33,11 +33,15 @@ namespace Collections {
         }
     }
 
-    export interface IEqual<T> {
-        equals(other: T): boolean;
+    export function defaultCompare<T>(lhs: T, rhs: T): number {
+        if (lhs < rhs) {
+            return -1;
+        } else if (lhs > rhs) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
-    export interface IOrdered<T> {
-        lessThan(other: T): boolean;
-    }
+    export type Comparator<T> = (lhs: T, rhs: T) => number;
 }
